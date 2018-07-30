23df2685dfd054289469d6191346891a292c2d9e
Di Yuan <dyuan@thoughtworks.com>
2018-07-30 16:40:22 +0800

    [YuanDi] use spring and xml properties to create bean

--- spring-app/src/main/java/com/thoughtworks/training/App.java
+++ spring-app/src/main/java/com/thoughtworks/training/App.java
@@ -1,15 +1,24 @@
 package com.thoughtworks.training;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        new App().run();
+        new App().runWithSpring();
     }
 
     public void run() {
         NumberProcessor numberProcessor = new NumberProcessor(Arrays.asList(new EvenFilter(), new PlusProcessor(2)));
+        System.out.println(numberProcessor.process(new Scanner(System.in).nextLine()));
+    }
+
+    public void runWithSpring() {
+        BeanFactory factory = new ClassPathXmlApplicationContext("content.xml");
+        NumberProcessor numberProcessor = factory.getBean(NumberProcessor.class);
         System.out.println(numberProcessor.process(new Scanner(System.in).nextLine()));
     }
 }
